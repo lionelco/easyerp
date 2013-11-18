f467c50d9341259bef66b2dc42c670aa67290498
Lionel <lionel.comandini@gmail.com>
2013-11-18 17:16:42 +0100

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Easyerp::Application.config.secret_token = '796b9ed6cc84b4a80f15d4e77c42e630d9aea9f8d1d2b61f4aa95f58264081fd1d018f43c2314e3ef5dd4be0be1e0e40e3b61e1e4348ee5f2280b054373d38f7'
+Easyerp::Application.config.secret_token = '81c5b51c388a5d1611d3d0b477c6f3058e1ceb1951d30d0e82e223f1ce3f82430acacf0265366ceaf6c6c055f26312ea1d9bd9ab4ea50263142c0a2ea58595d6'
