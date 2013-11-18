7afa3725d9dd3564594b8c30f38aa60e9f01ad14
Lionel <lionel.comandini@gmail.com>
2013-11-18 16:57:18 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyerp::Application.config.secret_token = '796b9ed6cc84b4a80f15d4e77c42e630d9aea9f8d1d2b61f4aa95f58264081fd1d018f43c2314e3ef5dd4be0be1e0e40e3b61e1e4348ee5f2280b054373d38f7'
